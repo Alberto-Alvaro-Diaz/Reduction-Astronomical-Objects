e588ff93b1879f3f43ef4453aee12dcdd26335c9
Alberto-Alvaro-Diaz <alberto.alvaro.diaz@gmail.com>
2024-12-20 09:44:54 +0100

    Update and rename Apilado.py to Stacking.py

--- Photometry/Stacking.py
+++ Photometry/Stacking.py
@@ -6,32 +6,28 @@ from astropy.visualization import make_lupton_rgb
 import re
 
 def rotate_matrix(matrix, angle):
-    # Convertir el ángulo a radianes
-    angle_rad = angle #ya está convertido
+    angle_rad = angle 
     
-    # Matriz de rotación en sentido antihorario
+    # Rotation Matrix conter clockwise 
     rotation_matrix = np.array([[np.cos(angle_rad), -np.sin(angle_rad)],
                                  [np.sin(angle_rad), np.cos(angle_rad)]])
-    
-    # Obtener las dimensiones de la matriz original
+    # Matrix shape
     rows, cols = matrix.shape
     
-    # Calcular el centro de la matriz
+    # Matrix center
     center = np.array([rows // 2, cols // 2])
     
-    # Crear una matriz para almacenar el resultado de la rotación
+    # New matrix where save the rotated one 
     rotated_matrix = np.zeros_like(matrix)
     
-    # Iterar sobre cada elemento de la matriz original
     for i in range(rows):
         for j in range(cols):
-            # Calcular la posición del elemento después de la rotación
+            # Calculate the new position for each element after rotation
             rotated_pos = np.dot(rotation_matrix, np.array([i, j]) - center) + center
             
-            # Redondear las posiciones y convertirlas a enteros
             rotated_i, rotated_j = np.round(rotated_pos).astype(int)
             
-            # Verificar si la posición rotada está dentro de los límites de la matriz original
+            # Verify if the new position is inside the limits
             if 0 <= rotated_i < rows and 0 <= rotated_j < cols:
                 # Asignar el valor de la matriz original a la posición rotada
                 rotated_matrix[rotated_i, rotated_j] = matrix[i, j]
@@ -39,9 +35,8 @@ def rotate_matrix(matrix, angle):
     return rotated_matrix
 
 def rotate_vec(vec, angle):
-    # Convertir el ángulo a radianes
-    centre=1601/2 #caso particular de la matriz 1601x1601
-    angle_rad = angle #ya está convertido
+    centre=1601/2 # particular case of matrix 1601x1601
+    angle_rad = angle 
     x1=vec[0]-centre
     y1=vec[1]-centre
     x=(np.cos(angle_rad)*x1 + np.sin(angle_rad)*y1)+centre
@@ -49,150 +44,141 @@ def rotate_vec(vec, angle):
     rotated_vec=np.array([x,y])
     return rotated_vec
 
+# To load the files .fits
+name_color='.\\data_folder_final_2' # Raw images inputs
+name_stacked='.\\data_folder_stacked_2' # Stacked images output
 
-#cargamos los fits
-name_color='.\\data_folder_final_2' #imagenes crudas 
-name_stacked='.\\data_folder_stacked_2' #imagenes apiladas output
-
-if not os.path.exists(name_stacked):  #carpeta para los flats
+if not os.path.exists(name_stacked):  # Folder flats
 	os.mkdir(os.path.join('.',name_stacked))
 
 final=[] 
-final=os.listdir(name_color) #imagenes crudas
+final=os.listdir(name_color) # Raw images
 
-map_color={re.findall('.\.', final[i])[0][0] : final[i] for i in range(0,len(final)) } #keys creadas 'i' 'r' 'g'
+map_color={re.findall('.\.', final[i])[0][0] : final[i] for i in range(0,len(final)) } # keys created 'i' 'r' 'g'
 
-#generamos las 3 imagenes de cada banda
+# Generating the 3 colour bands RGB
 R=(fits.open(os.path.join(name_color,map_color['i'])))[0].data
 G=(fits.open(os.path.join(name_color,map_color['r'])))[0].data
 B=(fits.open(os.path.join(name_color,map_color['g'])))[0].data
 
-#lista de las matrices que se van a utilizar
+# List of matrices that will be used
 color=[R,G,B]
 band=['R','G','B']
 coordinates1=[] #coordenadas estrella 1
 coordinates2=[] #coordenadas estrella 2
 
-print('A continuación deberás pinchas sobre las mismas estrellas para todas las bandas')
+print('The same star should be cliked for all bands')
 for i in range(len(color)):
     x=0 
-    while x != 'si':
-        print('Banda ' + band[i])
-        print('Pincha sobre una estrella del area inferior izquierda')
-        #ploteamos cada una de ellas y clickamos para coger pixeles de referencia
+    while x != 'yes':
+        print('Band ' + band[i])
+        print('A star in the lower left panel should be selected')
         plt.imshow(color[i],origin='lower',vmin=np.nanpercentile(color[i], 2), vmax=np.nanpercentile(color[i], 98))
         plt.colorbar()
         plt.title('Raw image in ' + band[i] + ' band')
         plt.show(block = False)
-        #clicamos en las estrella que queremos coger
+
         points_1=plt.ginput(1)
         plt.close()
-        #aplicamos zoom donde se ha clickado
+        # A zoom is applied where it was cliked
         cut=color[i][int(points_1[0][1])-50:int(points_1[0][1])+50,int(points_1[0][0])-50:int(points_1[0][0])+50]
         plt.imshow(cut,origin='lower',vmin=np.nanpercentile(cut, 2), vmax=np.nanpercentile(cut, 98))
         plt.colorbar()
         plt.title('Raw image in R band')
         plt.show(block = False)
-        #clicamos en las estrella que queremos coger
+
         points_11=plt.ginput(1)
         plt.close()
-        #calculamos las coordenadas sobre la imagen entera y no la recortada
+        # Calculating the coordinates
         centre=np.array(cut.shape)/2
         coordinates1.append(np.array(points_1[0])+np.array(points_11[0])-centre)
     
 
-        print('Pincha sobre una estrella del area superior derecha')
-        #ploteamos cada una de ellas y clickamos para coger pixeles de referencia
+        print('A star in the upper right panel should be selected')
         plt.imshow(color[i],origin='lower',vmin=np.nanpercentile(color[i], 2), vmax=np.nanpercentile(color[i], 98))
         plt.colorbar()
         plt.title('Raw image in R band')
         plt.show(block = False)
-        #clicamos en las estrella que queremos coger
+
         points_2=plt.ginput(1)
         plt.close()
-        #aplicamos zoom donde se ha clickado
+        # A zoom is applied
         cut=color[i][int(points_2[0][1])-50:int(points_2[0][1])+50,int(points_2[0][0])-50:int(points_2[0][0])+50]
         plt.imshow(cut,origin='lower',vmin=np.nanpercentile(cut, 2), vmax=np.nanpercentile(cut, 98))
         plt.colorbar()
         plt.title('Raw image in '+ band[i] +' band')
         plt.show(block = False)
-        #clicamos en las estrella que queremos coger
+
         points_22=plt.ginput(1)
         plt.close()
-        #calculamos las coordenadas sobre la imagen entera y no la recortada
+        # Calculating coordinates
         centre=np.array(cut.shape)/2
         coordinates2.append(np.array(points_2[0])+np.array(points_22[0])-centre)
 
-        print('Las coordenadas de ambas estrellas son: ')
+        print('The pixel coordinates of both stars are: ')
         print(coordinates1[i], coordinates2[i])
-        x=input('¿Conforme con la elección? (si/no)')
+        x=input('¿Are you agree with the choice? (yes/no)')
 
 
-#coordenadas de dos estrellas (abajo iz y arriba der de la imagen) 
-#del filtro R
+# star coordinates (lower left y upper right) 
+# band R
 n1_r=np.array([coordinates1[0][0],coordinates1[0][1]]) #np.array([595.5 , 418.4])
 n2_r=np.array([coordinates2[0][0],coordinates2[0][1]]) #np.array([1354.0 , 1172.6])
 
-#del filtro G
+# band G
 n1_g=np.array([coordinates1[1][0],coordinates1[1][1]]) #np.array([594.6 , 418.4])
 n2_g=np.array([coordinates2[1][0],coordinates2[1][1]]) #np.array([1353.2 , 1172.4])
 
-#del filtro B
+# band B
 n1_b=np.array([coordinates1[2][0],coordinates1[2][1]]) #np.array([621.8 , 403.3])
 n2_b=np.array([coordinates2[2][0],coordinates2[2][1]]) #np.array([1337.1 , 1198.7])
 
-#cálculo de vectores (final-inicial)
+# vector calculation
 vec_r=n2_r-n1_r
 vec_g=n2_g-n1_g
 vec_b=n2_b-n1_b
 
-#calculamos angulo (en rad)
-theta_r=np.arctan(vec_r[1]/vec_r[0]) #referencia
+# angle calculation (rad)
+theta_r=np.arctan(vec_r[1]/vec_r[0]) # reference
 theta_g=np.arctan(vec_g[1]/vec_g[0])
 theta_b=np.arctan(vec_b[1]/vec_b[0])
 
-theta_1=theta_g-theta_r #angulo que hay que rotar la matriz g
-theta_2=theta_b-theta_r #angulo que hay que rotar la matriz b
+theta_1=theta_g-theta_r # rotation angle for matrix g
+theta_2=theta_b-theta_r # rotation angle for matrix b
 
-#rotamos matrices teniendo R como referencia
+# Lets rotate the matrices, R matrix is chosen as reference
 rotated_g = rotate_matrix(G, theta_1)
 rotated_b = rotate_matrix(B, theta_2)
 
-#comenzamos desplazamiento en eje x e y
-#calculamos desplazamiento de los puntos calculados despues de la rotacion, primero rotamos los puntos
-#del filtro G
+# Lets boost the axis x and y
+# badn G
 n1_g_rotated=rotate_vec(n1_g, theta_1)
 n2_g_rotated=rotate_vec(n2_g, theta_1)
 
-#del filtro B
+# band B
 n1_b_rotated=rotate_vec(n1_b, theta_2)
 n2_b_rotated=rotate_vec(n2_b, theta_2)
 
 
-#calculamos desplazamiento dejando R como referencia
+# Calculating boost, R matrix is chosen as reference
 shift_g1=n1_g_rotated-n1_r
 shift_g2=n2_g_rotated-n2_r
 shift_b1=n1_b_rotated-n1_r
 shift_b2=n2_b_rotated-n2_r
 
-#media del desplazamiento
+# Boost mean
 shift_g=(shift_g1+shift_g2)/2
 shift_b=(shift_b1+shift_b2)/2
 
-#desplazamos matrices
-#banda g
-# Rotar cada fila de la matriz x posiciones hacia la arriba
+# band g
 for i in range(rotated_g.shape[0]):
     rotated_g[i, :] = np.roll(rotated_g[i, :], -round(shift_g[0]))
-# Rotar cada columna de la matriz x posiciones hacia la derecha
 for i in range(rotated_g.shape[1]):
     rotated_g[:, i] = np.roll(rotated_g[:, i], -round(shift_g[1]))
 
-#banda b
-# Rotar cada fila de la matriz x posiciones hacia la arriba
+# band b
 for i in range(rotated_b.shape[0]):
     rotated_b[i, :] = np.roll(rotated_b[i, :], -round(shift_b[0]))
-# Rotar cada columna de la matriz x posiciones hacia la derecha
 for i in range(rotated_b.shape[1]):
     rotated_b[:, i] = np.roll(rotated_b[:, i], -round(shift_b[1]))
 
@@ -217,6 +203,6 @@ band=['R','G','B']
 for i in range(len(matrix)):
     output_filename = os.path.join(name_stacked, 'stacked_'+ band[i] +'.fits')
     hdu = fits.PrimaryHDU(matrix[i])
-    hdu.writeto(output_filename, overwrite=True) #guardamos en la misma carpeta
+    hdu.writeto(output_filename, overwrite=True) # Saved it in the same folder
     print("Stacked image saved to", output_filename)
 
